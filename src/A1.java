33ba339d70fe58237ee9cf7f2dc7c553cd340132
Pat Sluth <pat.sluth@gmail.com>
2018-03-08 11:45:41 -0700

    Read args

--- src/A1.java
+++ src/A1.java
@@ -1,99 +1,66 @@
-// 	Pat Sluth
-// 	SENG 300
-//	30032750
-
-import org.apache.commons.lang.ObjectUtils.Null;
-import org.eclipse.core.internal.localstore.Bucket.Visitor;
-import org.eclipse.jdt.core.*;
-import org.eclipse.jdt.core.dom.*;
-
-import static org.junit.Assert.*;
+// 	SENG 300 Group A1
+//	Pat Sluth : 30032750
+//	Preston : XXXXXXX
+//	Ashton : XXXXXXX
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.InputStream;
-import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicInteger;
 
-import org.junit.Test;
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ASTParser;
+import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.FieldDeclaration;
+import org.eclipse.jdt.core.dom.Type;
 
 
 
 
 
-public class A1 
+public class A1
 {
-	public A1(String directoryPath, String typeName)
+	public void parse(String directoryPath, String filteredTypeName, AtomicInteger outDeclarationCount, AtomicInteger outReferenceCount)
 	{
+		outDeclarationCount.set(0);
+		outReferenceCount.set(0);
+		
+		
+		
 		File directoryFile = new File(directoryPath);
 		assert(directoryFile.isDirectory());
 		
+		
+		
+		ASTParser parser = ASTParser.newParser(AST.JLS3);
+		parser.setKind(ASTParser.K_COMPILATION_UNIT);
+		
+		
+		
 		for (File file : directoryFile.listFiles()) {
-//			System.out.println(file.getPath());
 			try {
 				String source = this.readFile(file);
 				System.out.println("Read file " + file.getPath());
-				
-				
-				
-				ASTParser parser = ASTParser.newParser(AST.JLS3);
-				parser.setKind(ASTParser.K_COMPILATION_UNIT);
+
 				parser.setSource(source.toCharArray());
 
 				CompilationUnit compilationUnit = (CompilationUnit)parser.createAST(null);
-				compilationUnit.accept(new FieldVisitor(typeName));
-				
-				
-				
+				compilationUnit.accept(new FieldDeclarationVisitor(new FieldDeclarationVisitor.Listener() {
+					@Override
+					public void didVisitNodeOfType(Type type) 
+					{
+						String typeName = type.toString();
+						
+						if (filteredTypeName == null || typeName.equals(filteredTypeName)) {
+							System.out.println("\t" + typeName);
+							outDeclarationCount.set(outDeclarationCount.get() + 1);
+						}
+					}
+				}));
 			} catch (FileNotFoundException e) {
 				System.out.println("Failed to reads file " + file.getPath());
 			}
-		}
-		
-//		try {
-//			String source = this.stringFromFile(filePath);
-//			//System.out.println(source);
-//			
-//			ASTParser parser = ASTParser.newParser(AST.JLS3);
-//
-//			parser.setKind(ASTParser.K_COMPILATION_UNIT);
-//			parser.setSource(source.toCharArray());
-//
-//			CompilationUnit compilationUnit = (CompilationUnit)parser.createAST(null);
-//			compilationUnit.accept(new ASTVisitor()
-//			{
-//				@Override
-//				public boolean visit(FieldDeclaration node) 
-//				{
-//
-////					Type t = node.getType();
-//					System.out.println(node.getType());
-//
-//					return super.visit(node);
-//				}
-//			}); 
-//			
-//		} catch (FileNotFoundException e) {
-//			System.out.printf("Failed to read file at %s\n", filePath);
-//		}
-	}
-	
-	private class FieldVisitor extends ASTVisitor
-	{
-		private String filteredTypeName = null;
-		
-		public FieldVisitor(String filteredTypeName) 
-		{
-			this.filteredTypeName = filteredTypeName;
-		}
-		
-		@Override
-		public boolean visit(FieldDeclaration node) 
-		{
-			if (node.getType().toString().equals(this.filteredTypeName)) {
-				System.out.println(node.getType());
-			}
-			return super.visit(node);
 		}
 	}
 	
@@ -114,13 +81,58 @@ public class A1
 	
 	
 	
+	
 	public static void main(String[] args) 
 	{
-		// TODO: get from args
 		String directoryPath = "/Volumes/Malish/Development/eclipse_workspace/CPSC441_A1/src";
-		String typeName = "String[]";
+		String typeName = null;
+		typeName = "int";
+//		typeName = "String[]";
 		
-		new A1(directoryPath, typeName);
+		if (args.length == 2) {
+			directoryPath = args[0];
+			typeName = args[1];
+		}
+		
+		A1 a1 = new A1();
+		AtomicInteger declarationCount = new AtomicInteger(0);
+		AtomicInteger referenceCount = new AtomicInteger(0);
+		a1.parse(directoryPath, typeName, declarationCount, referenceCount);
+		System.out.printf("declarationCount = %d\n", declarationCount.get());
+		System.out.printf("referenceCount = %d\n", referenceCount.get());
+	}
+}
+
+
+
+
+
+class FieldDeclarationVisitor extends ASTVisitor
+{	
+	interface Listener
+	{
+		void didVisitNodeOfType(Type type);
+	}
+	
+	private FieldDeclarationVisitor.Listener listener = null;
+	
+	
+	
+	
+	
+	public FieldDeclarationVisitor(FieldDeclarationVisitor.Listener listener) 
+	{
+		this.listener = listener;
+	}
+	
+	@Override
+	public boolean visit(FieldDeclaration node) 
+	{
+		if (this.listener != null) {
+			this.listener.didVisitNodeOfType(node.getType());
+		}
+		
+		return super.visit(node);
 	}
 }
 
